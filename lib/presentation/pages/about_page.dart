c8f779a77b9dc3c6ff24ccfcccee6e8f1f87e681
Randy Ahmad Garcia Santosa <randyahmad200@gmail.com>
2022-10-17 22:45:03 +0700

    Update Ditonton, Flutter Expert Project

--- lib/presentation/pages/about_page.dart
+++ lib/presentation/pages/about_page.dart
@@ -27,7 +27,7 @@ class AboutPage extends StatelessWidget {
                   padding: const EdgeInsets.all(32.0),
                   color: kMikadoYellow,
                   child: Text(
-                    'Ditonton adalah sebuah aplikasi katalog film yang dikembangkan oleh Dicoding Indonesia sebagai contoh proyek aplikasi untuk kelas Menjadi Flutter Developer Expert.',
+                    'Ditonton merupakan sebuah aplikasi katalog film yang dikembangkan oleh Dicoding Indonesia sebagai contoh proyek aplikasi untuk kelas Menjadi Flutter Developer Expert.',
                     style: TextStyle(color: Colors.black87, fontSize: 16),
                     textAlign: TextAlign.justify,
                   ),
